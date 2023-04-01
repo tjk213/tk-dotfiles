22307e6066cf501df12582e838b5e01b361cfdc6
Tyler J. Kenney <tjk213@gmail.com>
2023-04-01 17:20:39 +0000

    [tmux] Add check for pre-existing session

--- tmux.sh
+++ tmux.sh
@@ -17,21 +17,26 @@
 ####################################################################################
 ####################################################################################
 
+# Attach to session "default" if it exists.
+# Otherwise, create it and attach.
 function tmux-default()
 {
-    # Start default session
-    #
-    # x & y set the width & height of the session, which otherwise is uninferred
-    # until the session is first attached. This is necessary for resizing panes
-    # below to work correctly.
-    tmux new-session -ds 'default' -x $COLUMNS -y $LINES
+    DEFAULT_SESSION="default"
+    tmux has-session -t $DEFAULT_SESSION &> /dev/null
 
-    # Start htop pane on bottom
-    tmux split-window -v
-    tmux send-keys 'htop' C-m
-    tmux resize-pane -y 11 # Assuming 4 rows of CPU meters, should show 0 processes
-    tmux last-pane
+    if [[ $? != 0 ]] ; then
+	# x & y set the width & height of the session, which otherwise is uninferred
+	# until the session is first attached. This is necessary for resizing panes
+	# below to work correctly.
+	tmux new-session -ds $DEFAULT_SESSION -x $COLUMNS -y $LINES
+
+	# Start htop pane on bottom
+	tmux split-window -v
+	tmux send-keys 'htop' C-m
+	tmux resize-pane -y 11 # Assuming 4 rows of CPU meters, shows 0 processes
+	tmux last-pane
+    fi
 
     # Attach!
-    tmux attach -t 'default'
+    tmux attach -t $DEFAULT_SESSION
 }
