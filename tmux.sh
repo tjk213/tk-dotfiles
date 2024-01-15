aabc9b61c526956e0b3d030a0a2c6e140a08b4cb
Tyler J. Kenney <tjk213@gmail.com>
2024-01-15 10:38:25 -0500

    [tmux] Refactor & add tmux-nv

--- tmux.sh
+++ tmux.sh
@@ -38,6 +38,61 @@ function htop-pane-height()
     echo $(($num_header_rows+$num_cpu_rows+$num_trailing_rows))
 }
 
+function tmux-new-session()
+{
+    SESSION=$1
+    # x & y set the width & height of the session, which otherwise is uninferred
+    # until the session is first attached. This is necessary for resizing panes
+    # below to work correctly.
+    tmux new-session -ds $SESSION -x $COLUMNS -y $LINES
+}
+
+function tmux-config-modular()
+{
+    #
+    # Clear TMOUT variable, if its set. Modular VMs have this set by default to
+    # kill shells after two hours of inactivity. This kills any idle SSH
+    # connections which triggers auto-shutdown after two more hours of inactivity.
+    # However, we don't want tmux panes to timeout because 1.) they won't prevent
+    # auto-shutdown (the SSH connect still gets killed because the 'parent' shell
+    # is still determined to be idle, even if there is a tmux session open within
+    # it) and 2.) if we log back in after the SSH connection gets killed but
+    # before the auto-shutdown triggers, we want to be able to re-attach to our
+    # tmux session and have everything the way we left it.
+    #
+    # NOTE: On modular VMs, TMOUT is marked readonly. This will fail if we haven't
+    #       manually updated /etc/zsh/zshrc.
+    #
+    tmux send-keys 'unset TMOUT' C-m
+    tmux send-keys 'clear' C-m
+}
+
+function tmux-config-cpu()
+{
+    # Start htop pane
+    tmux split-window -v
+    tmux send-keys 'htop' C-m
+    tmux resize-pane -y $(htop-pane-height)
+    tmux last-pane
+}
+
+function tmux-config-gpu()
+{
+    tmux split-window -v
+    tmux send-keys 'nvtop' C-m
+    # We would prefer to resize this pane directly with -y 3, but this unfortunately
+    # adds the new space to the pane below rather than the pane above. There may be
+    # a method for specifying the "primary" pane or something like that, but I'm not
+    # aware of it. Instead, we switch back to the primary pane and resize it bigger,
+    # which shrinks the GPU pane down to desired size.
+    tmux last-pane
+    DIVIDERS=2
+    GPU_PANE_SIZE=3
+    CPU_PANE_SIZE=$(htop-pane-height)
+    PRIMARY_PANE_SIZE=$(($LINES-$CPU_PANE_SIZE-$GPU_PANE_SIZE-$DIVIDERS))
+    tmux resize-pane -y $PRIMARY_PANE_SIZE
+}
+
 # Attach to session "default" if it exists.
 # Otherwise, create it and attach.
 function tmux-default()
@@ -46,35 +101,26 @@ function tmux-default()
     tmux has-session -t $DEFAULT_SESSION &> /dev/null
 
     if [[ $? != 0 ]] ; then
-	# x & y set the width & height of the session, which otherwise is uninferred
-	# until the session is first attached. This is necessary for resizing panes
-	# below to work correctly.
-	tmux new-session -ds $DEFAULT_SESSION -x $COLUMNS -y $LINES
-
-	#
-	# Clear TMOUT variable, if its set. Modular VMs have this set by default to
-	# kill shells after two hours of inactivity. This kills any idle SSH
-	# connections which triggers auto-shutdown after two more hours of inactivity.
-	# However, we don't want tmux panes to timeout because 1.) they won't prevent
-	# auto-shutdown (the SSH connect still gets killed because the 'parent' shell
-	# is still determined to be idle, even if there is a tmux session open within
-	# it) and 2.) if we log back in after the SSH connection gets killed but
-	# before the auto-shutdown triggers, we want to be able to re-attach to our
-	# tmux session and have everything the way we left it.
-	#
-	# NOTE: On modular VMs, TMOUT is marked readonly. This will fail if we haven't
-	#       manually updated /etc/zsh/zshrc.
-	#
-        tmux send-keys 'unset TMOUT' C-m
-        tmux send-keys 'clear' C-m
-
-	# Start htop pane on bottom
-	tmux split-window -v
-	tmux send-keys 'htop' C-m
-	tmux resize-pane -y $(htop-pane-height)
-	tmux last-pane
+	tmux-new-session $DEFAULT_SESSION
+	tmux-config-modular
+	tmux-config-cpu
     fi
 
     # Attach!
     tmux attach -t $DEFAULT_SESSION
+}
+
+function tmux-nv()
+{
+    SESSION="nv"
+    tmux has-session -t $SESSION &> /dev/null
+
+    if [[ $? != 0 ]] ; then
+	tmux-new-session $SESSION
+	tmux-config-cpu
+	tmux-config-gpu
+    fi
+
+    # Attach!
+    tmux attach -t $SESSION
 }
