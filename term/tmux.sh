c4e101f6400a058d08eaae7ad6553059b7bf0df4
Tyler Kenney <tjk213@proton.me>
2026-01-08 09:25:49 -0500

    [term] Drop old modular stuff from MDCM days

--- term/tmux.sh
+++ term/tmux.sh
@@ -89,26 +89,6 @@ function tmux-new-session()
     tmux new-session -ds $SESSION -x $COLUMNS -y $LINES
 }
 
-function tmux-config-modular()
-{
-    #
-    # Clear TMOUT variable, if its set. Modular VMs have this set by default to
-    # kill shells after two hours of inactivity. This kills any idle SSH
-    # connections which triggers auto-shutdown after two more hours of inactivity.
-    # However, we don't want tmux panes to timeout because 1.) they won't prevent
-    # auto-shutdown (the SSH connect still gets killed because the 'parent' shell
-    # is still determined to be idle, even if there is a tmux session open within
-    # it) and 2.) if we log back in after the SSH connection gets killed but
-    # before the auto-shutdown triggers, we want to be able to re-attach to our
-    # tmux session and have everything the way we left it.
-    #
-    # NOTE: On modular VMs, TMOUT is marked readonly. This will fail if we haven't
-    #       manually updated /etc/zsh/zshrc.
-    #
-    tmux send-keys 'unset TMOUT' C-m
-    tmux send-keys 'clear' C-m
-}
-
 function tmux-config-cpu()
 {
     # Start htop pane
@@ -162,7 +142,6 @@ function tmux-default()
 
     if [[ $? != 0 ]] ; then
 	tmux-new-session $DEFAULT_SESSION
-	tmux-config-modular
 	tmux-config-cpu
     fi
 
