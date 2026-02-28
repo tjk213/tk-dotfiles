634ca1391d721bb000052dfde95c2a96a0dc92b2
Tyler Kenney <tjk213@proton.me>
2026-02-27 23:18:25 -0500

    [core] Filter out macbook builtin display

--- core/swap-sources.sh
+++ core/swap-sources.sh
@@ -35,17 +35,19 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     # which doesn't have /opt in its PATH.
     m1ddc=/opt/homebrew/bin/m1ddc
 
-    # Get number of displays
-    num_displays=$($m1ddc display list | wc -l)
-
-    # Switch all screens to DisplayPort (0x0f)
+    # Switch all "non-null" screens to DisplayPort (0x0f)
+    # On my modular M1 macbook, m1ddc display list shows the following:
+    #
+    #  [1] (null) (37D8832A-2D66-02CA-B9F7-8F30A301B230)
+    #  [2] DELL U2723QE (E522360D-B4DA-4D42-87EB-C10B031FE17B)
+    #  [3] DELL U2723QE (0A1F4811-4624-44B6-A9B0-8497FF53D33E)
+    #
+    # ...the null screen is the builtin display. We don't want to / cannot switch it
+    # so we filter it out with grep.
     # TODO: Generalize this
     #   - we should at least check to see if the current display has a DP connection
     #   - it would be good to parameterize so we can switch to any desired input
-    # E.g., on the Modular M1 laptop this will send an input code to builtin apple
-    # display. As far as I can tell this does no harm, but it might be one part of
-    # all the window resizing issues.
-    for i in $(seq $num_displays); do
+    $m1ddc display list | grep -vn "null" | cut -d: -f1 | while read i; do
 	$m1ddc display $i set input 15
 	sleep 1 # m1ddc doesn't like operating at a high frequency for some reason.
     done
